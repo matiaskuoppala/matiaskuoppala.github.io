5ffd1b4959dd2c4e75f95361730faa7a00fa817c
matiaskuoppala <matias.kuoppala@aalto.fi>
2017-04-10 17:44:57 +0300

    mm

--- player.js
+++ player.js
@@ -1,28 +1,40 @@
-var x = 0;
-var y = 0;
-var speed = defaultSpeed;
-var defaultSpeed = 3;
-var img = document.getElementById("pImg")
+var x = 184;
+var y = 200;
+var speed = 5;
 //Muuta?
 
-function move(direction: String) {
+function move(direction) {
+    if (x >= 0 && y >= 0 && y + 33 <= canvas.height && x + 33 <= canvas.width) {
     if (direction == "left") {
         x -= speed;
-        draw(c, x, y)
+        draw(ctx, x, y, spIdx);
     } else if (direction == "right") {
         x += speed;
-        draw(c, x, y)
+        draw(ctx, x, y, spIdx)
     } else if (direction == "up") {
-        y += speed;
-        draw(c, x, y)
-    } else if (direction == "down") {
         y -= speed;
-        draw(c, x, y)
+        draw(ctx, x, y, spIdx);
+    } else if (direction == "down") {
+        y += speed;
+        draw(ctx, x, y, spIdx);
+    } 
+    } else if (x < 0) {
+        x = 1;
+        draw(ctx, x, y, spIdx);
+    } else if (x + 32 > canvas.width) {
+        x = canvas.width - 33;
+        draw(ctx, x, y, spIdx);
+    } else if (y < 0) {
+        y = 1;
+        draw(ctx, x, y, spIdx);
+    } else if (y + 32 > canvas.height) {
+        y = canvas.height - 33;
+        draw(ctx, x, y, spIdx);
     }
 }
 
-function changeSpeed(newSpeed: Int) {
-    speed = newSpeed
+function changeSpeed(newSpeed) {
+    speed = newSpeed;
 }
 
 function reset() {
@@ -31,6 +43,6 @@ function reset() {
     speed = defaultSpeed;
 }
 
-function draw(c, x: Int, y: Int) {
-    c.drawImage(img, x, y)
+function draw(c, x, y, sIdx) {
+    c.drawImage(img, 32*sIdx, 0, 32, 32, x, y, 32, 32);
 }
